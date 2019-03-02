da669893052b151d0f4badeba2445208763a4cc5
prophet <prophetmanx@gmail.com>
2019-03-02 01:58:19 -0500

    added comments to the base destination class

--- BaseLoggingDestination.cs
+++ BaseLoggingDestination.cs
@@ -2,26 +2,45 @@ using System;
 
 namespace ProphetsWay.Logger{
 
+	/// <summary>
+	/// A Base class for creating new ILoggingDestinations, it is thread safe and will check the level of a message before logging.
+	/// </summary>
 	public abstract class BaseLoggingDestination : ILoggingDestination
 	{
-		public BaseLoggingDestination(LogLevels reportingLevel){
+		protected BaseLoggingDestination(LogLevels reportingLevel){
 			_reportingLevel = reportingLevel;
 		}
 
 		protected readonly object LoggerLock = new object();
 		private readonly LogLevels _reportingLevel;
 
-
+		/// <summary>
+		/// This is the required method from the ILoggingDestinations interface, this is the entry point for the destination from the Logger static class.
+		/// </summary>
 		public void Log(LogLevels level, string message = null, Exception ex = null)
 		{
+			//if the level of the message being passed is lower than the ReportingLevel set on the Destination, then return and don't log the message
 			if((level & _reportingLevel) < level)
 				return;
 
 			MassageLogStatement(level, message, ex);
 		}
 
+		/// <summary>
+		/// This is the method you must implement in your custom LoggingDestination.  
+		/// It is where you will actually write the given message wherever you want.
+		/// This is called from inside the Destination whenever there is a valid message to write to the log.
+		/// </summary>
+		/// <param name="message">The message being logged.</param>
+		/// <param name="level">The LogLevel of the message being passed.</param>
 		protected abstract void WriteLogEntry(string message, LogLevels level);
 
+		/// <summary>
+		/// This method is meant to massage the statement to be logged, before it goes to the WriteLogEntry method.
+		/// It will extract the details from an exception and add them to the end of the message.
+		/// It is Virtual and can be overridden if you wish to customize how you handle writing the raw Log method call.
+		/// This method calls WriteLogEntry.
+		/// </summary>
 		protected virtual void MassageLogStatement(LogLevels level, string message = null, Exception ex = null){
 			string exMessage, exStackTrace;
 
@@ -42,6 +61,9 @@ namespace ProphetsWay.Logger{
 			WriteLogEntry(message, level);
 		}
 
+		/// <summary>
+		/// Recursive function to peer deep into an Exception and its Inner Exceptions to capture the messages and stack traces within.
+		/// </summary>
 		private static void ExceptionDetailer(Exception ex, out string message, out string stack){
 			var imessage = string.Empty;
 			var istack = string.Empty;
