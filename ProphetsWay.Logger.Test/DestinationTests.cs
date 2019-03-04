cb6f375038066b301b9c855d440ee6d59c15d817
prophet <prophetmanx@gmail.com>
2019-03-03 21:14:44 -0500

    cleaned out some things i was adding in the wrong branch

--- ProphetsWay.Logger.Test/DestinationTests.cs
+++ ProphetsWay.Logger.Test/DestinationTests.cs
@@ -13,16 +13,12 @@ namespace ProphetsWay.Logger.Test
 		}
 
 		[Theory]
-		[InlineData(LogLevels.NoLogging, 1, 1, 2, 2, 2, 2)]
+		[InlineData(LogLevels.NoLogging, 1, 1, 1, 1, 1, 1)]
 		[InlineData(LogLevels.Error, 0, 1, 1, 1, 1, 1)]
-		[InlineData(LogLevels.Warning, 0, 0, 2, 1, 1, 1)]
-		[InlineData(LogLevels.Security, 0, 0, 0, 2, 1, 1)]
-		[InlineData(LogLevels.Information, 0, 0, 0, 0, 2, 1)]
-		[InlineData(LogLevels.Debug, 0, 0, 0, 0, 0, 2)]
-		[InlineData(LogLevels.WarningOnly, 0, 0, 2, 0, 0, 0)]
-		[InlineData(LogLevels.SecurityOnly, 0, 0, 0, 2, 0, 0)]
-		[InlineData(LogLevels.InformationOnly, 0, 0, 0, 0, 2, 0)]
-		[InlineData(LogLevels.DebugOnly, 0, 0, 0, 0, 0, 2)]
+		[InlineData(LogLevels.Warning, 0, 0, 1, 1, 1, 1)]
+		[InlineData(LogLevels.Security, 0, 0, 0, 1, 1, 1)]
+		[InlineData(LogLevels.Information, 0, 0, 0, 0, 1, 1)]
+		[InlineData(LogLevels.Debug, 0, 0, 0, 0, 0, 1)]
 		public void LogShouldHaveFallThruCounts(LogLevels level, int no, int err, int warn, int sec, int info, int debug)
 		{
 			//setup
@@ -42,10 +38,6 @@ namespace ProphetsWay.Logger.Test
 		[InlineData(LogLevels.Security)]
 		[InlineData(LogLevels.Information)]
 		[InlineData(LogLevels.Debug)]
-		[InlineData(LogLevels.WarningOnly)]
-		[InlineData(LogLevels.SecurityOnly)]
-		[InlineData(LogLevels.InformationOnly)]
-		[InlineData(LogLevels.DebugOnly)]
 		public void LogShouldHaveMessage(LogLevels targetLevel)
 		{
 			//setup
@@ -68,12 +60,12 @@ namespace ProphetsWay.Logger.Test
 		{
 			InitTest();
 			Utilities.Logger.Log(LogLevels.NoLogging, "Hello World", new Exception("Custom Exception"));
-			AssertCounts(1, 1, 2, 2, 2, 2);
+			AssertCounts(1, 1, 1, 1, 1, 1);
 
 
 			Utilities.Logger.Log(LogLevels.NoLogging, "Hello World", new Exception("Custom Exception"));
 			Utilities.Logger.ClearDestinations();
-			AssertCounts(1, 1, 2, 2, 2, 2);
+			AssertCounts(1, 1, 1, 1, 1, 1);
 
 
 			var dest = new EventDestination(LogLevels.NoLogging);
@@ -81,7 +73,7 @@ namespace ProphetsWay.Logger.Test
 
 
 			Utilities.Logger.Log(LogLevels.NoLogging, "Hello World", new Exception("Custom Exception"));
-			AssertCounts(2, 1, 1, 1, 1, 1);
+			AssertCounts(1, 1, 1, 1, 1, 1);
 		}
 
 
@@ -105,32 +97,13 @@ namespace ProphetsWay.Logger.Test
 
 			var dd = new EventDestination(LogLevels.Debug);
 			dd.LoggingEvent += (sender, args) => _dCount++;
-
-
-
-			var wod = new EventDestination(LogLevels.WarningOnly);
-			wod.LoggingEvent += (sender, args) => _wCount++;
-
-			var sod = new EventDestination(LogLevels.SecurityOnly);
-			sod.LoggingEvent += (sender, args) => _sCount++;
-
-			var iod = new EventDestination(LogLevels.InformationOnly);
-			iod.LoggingEvent += (sender, args) => _iCount++;
-
-			var dod = new EventDestination(LogLevels.DebugOnly);
-			dod.LoggingEvent += (sender, args) => _dCount++;
-
+			
 			Utilities.Logger.AddDestination(nd);
 			Utilities.Logger.AddDestination(ed);
 			Utilities.Logger.AddDestination(wd);
 			Utilities.Logger.AddDestination(sd);
 			Utilities.Logger.AddDestination(id);
 			Utilities.Logger.AddDestination(dd);
-
-			Utilities.Logger.AddDestination(wod);
-			Utilities.Logger.AddDestination(sod);
-			Utilities.Logger.AddDestination(iod);
-			Utilities.Logger.AddDestination(dod);
 		}
 
 		private int _nCount = 0;
