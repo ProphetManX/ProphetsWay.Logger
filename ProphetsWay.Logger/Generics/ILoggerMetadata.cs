234daea1ba1f2a55f0f6aefb0ea55a378459cd8c
G. Gordon Nasseri <ghnasser@microsoft.com>
2019-04-18 13:21:04 -0400

    renamed the context to metadata, as that was the original naming convention used in the project.
    got the unit tests created and working for the new extensions

--- ProphetsWay.Logger/Generics/ILoggerMetadata.cs
+++ ProphetsWay.Logger/Generics/ILoggerMetadata.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// This is a class designed to identify classes as something specific that you're going to use with Generic Logger messages
     /// </summary>
-    public interface ILoggerContext
+    public interface ILoggerMetadata
     {
         //intentionally left blank, this is just to identify your class as an object you're going to want to log as metadata
     }
---
+++ ProphetsWay.Logger/LoggerDestinations/MetadataExtensions.cs
@@ -0,0 +1,38 @@
+﻿using ProphetsWay.Utilities.Generics;
+using System;
+
+namespace ProphetsWay.Utilities.LoggerDestinations
+{
+    public static class MetadataExtensions
+    {
+        public static void Log<T>(this T metadata, LogLevels level, string message = null, Exception ex = null) where T : ILoggerMetadata
+        {
+            Logger.Log(level, metadata, message, ex);
+        }
+
+        public static void Debug<T>(this T metadata, string message = null) where T : ILoggerMetadata
+        {
+            Logger.Debug(message, metadata);
+        }
+
+        public static void Info<T>(this T metadata, string message = null) where T : ILoggerMetadata
+        {
+            Logger.Info(message, metadata);
+        }
+
+        public static void Security<T>(this T metadata, string message = null) where T : ILoggerMetadata
+        {
+            Logger.Security(message, metadata);
+        }
+
+        public static void Warn<T>(this T metadata, string message = null, Exception ex = null) where T : ILoggerMetadata
+        {
+            Logger.Warn(message, metadata, ex);
+        }
+
+        public static void Error<T>(this T metadata, Exception ex = null, string message = null) where T : ILoggerMetadata
+        {
+            Logger.Error(ex, metadata, message);
+        }
+    }
+}
